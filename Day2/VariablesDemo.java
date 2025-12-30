b0dc749890e833bd853df5b614cdf3eee15556e0
Nani6238 <pnani6238@gmail.com>
2025-12-30 15:02:47 +0530

    Add files via upload
    Added Day2 java programs

---
+++ Day2/VariablesDemo.java
@@ -0,0 +1,12 @@
+class variablesDemo
+{
+public static void main(String args[])
+{
+String name="RaviTeja";
+int age=19;
+double grade=8.3;
+System.out.println("Name"+name);
+System.out.println("age"+age);
+System.out.println("grade"+grade);
+}
+}
